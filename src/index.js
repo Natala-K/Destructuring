b0eb84195f9d56051adea5a6f09f8e8675a303cf
Kolmy <test@neto.com>
2024-07-27 20:53:23 +0300

    added

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// Функция для извлечения специальных атак персонажа
+export function getSpecialAttacks({ special }) {
+    return special.map(({ id, name, icon, description = 'Описание недоступно' }) => ({
+      id,
+      name,
+      icon,
+      description,
+    }));
+  }
+  
+  // Пример использования функции
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      }, 
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...'
+        // описание отсутствует
+      }
+    ]
+  };
+  
+  console.log(getSpecialAttacks(character));
+  
